70868e3e4e9474dc012d6575a96fcc6c84e84847
u202415820 <u202415820@upc.edu.pe>
2026-04-08 10:55:03 -0500

    feat(greeting): add get and post endpoints for greetings

--- Acme.Hello.Platform/Program.cs
+++ Acme.Hello.Platform/Program.cs
@@ -1,3 +1,7 @@
+using Acme.Hello.Platform.Generic.Domain.Model.Entities;
+using Acme.Hello.Platform.Generic.interfaces.REST.Assemblers;
+using Acme.Hello.Platform.Generic.interfaces.REST.Resources;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +15,20 @@ if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
+app.MapGet("/greetings", (string? firstName, string? lastName) =>
+    {
+    var developer = !string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName)
+        ? new Developer(firstName, lastName)
+        : null;
+    var response = GreetDeveloperAssembler.ToResponseFromEntity(developer);
+    return Results.Ok(response);
+    }).WithName("GetGreeting");
 
+app.MapPost("/greetings", (GreetDeveloperRequest request) =>
+    {
+        var developer = DeveloperAssembler.ToEntityFromRequest(request);
+        var response = GreetDeveloperAssembler.ToResponseFromEntity(developer);
+    return Results.Created("/greetings", response);
+    });
 
 app.Run();
